bd9c50b2bfebcc7710bb145c83bcc19f2ac61c5e
Atlas Vlad <vlad269123@gmail.com>
2025-03-03 14:45:43 +0300

    radio

--- src/test/java/ru/netology/statistic/TestRadio.java
+++ src/test/java/ru/netology/statistic/TestRadio.java
@@ -1,9 +1,55 @@
 package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestRadio {
+
+    @Test
+    public void defaultConstructorShouldSet10Stations() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+        radio.next();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldCreateRadioWithCustomStationCount() {
+        Radio radio = new Radio(5);
+        radio.setCurrentStation(4);
+        radio.next();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldNotSetStationBeyondCustomCount() {
+        Radio radio = new Radio(5);
+        radio.setCurrentStation(5);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldSwitchFromMaxTo0OnNext() {
+        Radio radio = new Radio(5);
+        radio.setCurrentStation(4);
+        radio.next();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldSwitchFrom0ToMaxOnPrev() {
+        Radio radio = new Radio(5);
+        radio.setCurrentStation(0);
+        radio.prev();
+        assertEquals(4, radio.getCurrentStation());
+    }
+
+    @Test
+    public void invalidStationCountShouldThrow() {
+        assertThrows(IllegalArgumentException.class, () -> new Radio(0));
+        assertThrows(IllegalArgumentException.class, () -> new Radio(-5));
+    }
+
 
     @Test
     public void shouldSetStation() {
@@ -20,108 +66,10 @@ public class TestRadio {
     }
 
     @Test
-    public void shouldNotSetStationAbove9() {
+    public void shouldNotSetStationAboveMax() {
         Radio radio = new Radio();
         radio.setCurrentStation(10);
         assertEquals(0, radio.getCurrentStation());
     }
 
-    @Test
-    public void shouldSwitchFrom9To0() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(9);
-        radio.next();
-        assertEquals(0, radio.getCurrentStation());
-    }
-
-    @Test
-    public void shouldSwitchFrom8To9() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(8);
-        radio.next();
-        assertEquals(9, radio.getCurrentStation());
-    }
-
-    @Test
-    public void shouldSwitchFrom0To9() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(0);
-        radio.prev();
-        assertEquals(9, radio.getCurrentStation());
-    }
-
-    @Test
-    public void shouldSwitchFrom1To0() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(1);
-        radio.prev();
-        assertEquals(0, radio.getCurrentStation());
-    }
-
-    @Test
-    public void shouldIncreaseVolume() {
-        Radio radio = new Radio();
-        radio.increaseVolume();
-        assertEquals(1, radio.getCurrentVolume());
-    }
-
-    @Test
-    public void shouldNotIncreaseVolumeAbove100() {
-        Radio radio = new Radio();
-        for (int i = 0; i < 100; i++) {
-            radio.increaseVolume();
-        }
-        assertEquals(100, radio.getCurrentVolume());
-        radio.increaseVolume();
-        assertEquals(100, radio.getCurrentVolume());
-    }
-
-    @Test
-    public void shouldDecreaseVolume() {
-        Radio radio = new Radio();
-        radio.increaseVolume();
-        radio.decreaseVolume();
-        assertEquals(0, radio.getCurrentVolume());
-    }
-
-    @Test
-    public void shouldNotDecreaseVolumeBelow0() {
-        Radio radio = new Radio();
-        radio.decreaseVolume();
-        assertEquals(0, radio.getCurrentVolume());
-    }
-
-    @Test
-    public void shouldKeepCurrentStationWhenSetInvalid() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(5);
-        radio.setCurrentStation(-1);
-        assertEquals(5, radio.getCurrentStation());
-        radio.setCurrentStation(10);
-        assertEquals(5, radio.getCurrentStation());
-    }
-
-    @Test
-    public void testMultipleNextPresses() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(8);
-        radio.next();
-        assertEquals(9, radio.getCurrentStation());
-        radio.next();
-        assertEquals(0, radio.getCurrentStation());
-        radio.next();
-        assertEquals(1, radio.getCurrentStation());
-    }
-
-    @Test
-    public void testMultiplePrevPresses() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(1);
-        radio.prev();
-        assertEquals(0, radio.getCurrentStation());
-        radio.prev();
-        assertEquals(9, radio.getCurrentStation());
-        radio.prev();
-        assertEquals(8, radio.getCurrentStation());
-    }
 }
